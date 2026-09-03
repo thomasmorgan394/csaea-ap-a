c9cea3b411d4626be3a8df5d7e5cb3519248f84b
HingleMcringleberryTM <thomasolivermorgan09@gmail.com>
2026-09-03 13:38:19 +0000

    f

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -1,5 +1,7 @@
 package CrashCourse;
 
+import javax.print.DocFlavor.STRING;
+
 public class CrashCourse { // class header 
     public static void main(String[] args) {
         // comments 
@@ -45,13 +47,48 @@ public class CrashCourse { // class header
         // logical operators
         // In order of precedence NOT: ! AND: && OR: ||
 
-        boolean e  = false; boolean f = true;
-        System.out.println(!e);
-        System.out.println(e && f);
-        System.out.println(e || f && !e);
+        boolean t  = true; boolean f = false;
+        System.out.println(!t);
+        System.out.println(t && f);
+        System.out.println(t || f && !t); //short circuits after checking the first truth
         // short circuits after checking e -- java does not need to check f.
+        // CASTING (converting)
 
+        int g = (int)5.5; //CASTING
+        System.out.println(g);
 
+        double h = (double)5/6; //CASTING (the 2nd double is the caster)
+        System.out.println(h);
+
+        // STRINGS
+        String s1 = "Goodnight";
+        String s2 = " and";
+        String s3 = " Goodbye";
+        String result = s1 + s2 + s3;
+        result += ", Cowboy. ";
+        System.out.println(result + "\n");
+
+        // 
+        int[] arry1 = new int[10];
+        System.out.println(arry1);
+        System.out.println("length: " + arry1.length);
+        arry1[0] = 11;
+        arry1[1] = 2;
+
+        System.out.println("Remainder: " + arry1[0] % arry1[1]);
+        int[] arry2 = {34, 52, 3, 64, 32};
+        System.out.println("arry2: " + arry2);
+        // 2D arrays (grid or a table)
+
+        int[][] arryGrid1 = new int[4][3]; //[rows][columns]
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0 
+
+        
+        System.out.println("Rows:" + arryGrid1.length);
+        System.out.println("Columns:" + arryGrid1[0].length);
 
     }
 }
