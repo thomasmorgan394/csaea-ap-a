0828d80bde0aff547e098453d959354d708dbc5f
HingleMcringleberryTM <thomasolivermorgan09@gmail.com>
2026-09-15 12:25:01 +0000

    new commit

---
+++ CrashCourse/Dog.java
@@ -0,0 +1,25 @@
+package CrashCourse;
+
+public class Dog { 
+
+    // Data // atributes 
+String name;
+String ownerName;
+int age; 
+int energyLevel;
+String breed;
+boolean isHungry;
+Double weight;
+Boolean isHome;
+
+public Dog(String name,String ownerName,String breed) { 
+    this.name = name;
+    this.ownerName = ownerName;
+    this.breed = breed;
+    }
+// behaviors / methods
+public void eat() {
+    
+}
+    
+}
---
+++ CrashCourse/Series.java
@@ -0,0 +1,30 @@
+package CrashCourse;
+
+public class Series { 
+
+    // Data // atributes 
+String showName;
+String genre;
+int episodes#; 
+int seasons#;
+String actors;
+String director;
+Double imdbRating;
+String qualityOption;
+
+public Series(String director,String genre,String showName) { 
+    this.showName = showName;
+    this.director = director;
+    this.genre = genre;
+
+    imdbRating = 8.0;
+    seasons# = 3;
+    episodes# = 24;
+}
+    Int numRatings;
+    public void rate() {
+        imdbRating = imdbRating += rate / 
+    }
+
+    
+}
