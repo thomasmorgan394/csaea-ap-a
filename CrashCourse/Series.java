606b9e73ea821a684a05e8cf024898ea1e8a9ba8
HingleMcringleberryTM <thomasolivermorgan09@gmail.com>
2026-09-15 12:36:30 +0000

    finished

--- CrashCourse/Series.java
+++ CrashCourse/Series.java
@@ -5,26 +5,26 @@ public class Series {
     // Data // atributes 
 String showName;
 String genre;
-int episodes#; 
-int seasons#;
+int episodes; 
+int seasons;
 String actors;
 String director;
 Double imdbRating;
 String qualityOption;
-
+int numRatings;
+int rate;
+int episodeOn;
 public Series(String director,String genre,String showName) { 
     this.showName = showName;
     this.director = director;
     this.genre = genre;
-
     imdbRating = 8.0;
-    seasons# = 3;
-    episodes# = 24;
-}
-    Int numRatings;
-    public void rate() {
-        imdbRating = imdbRating += rate / 
-    }
+    seasons = 3;
+    episodes = 24; 
+} 
+    public void finish() {
+         episodes = episodeOn ++;
+    } 
+        
 
-    
-}
+    }
