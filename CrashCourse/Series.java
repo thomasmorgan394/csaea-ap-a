fa68408c3febfa7101b9d46bee99321cf75b4811
HingleMcringleberryTM <thomasolivermorgan09@gmail.com>
2026-09-18 02:28:12 +0000

    homework

--- CrashCourse/Series.java
+++ CrashCourse/Series.java
@@ -3,11 +3,11 @@ package CrashCourse;
 public class Series {
 private String showName;
 private String genre;
-int episodes; 
+private int episodes; 
 int seasons;
 String actors;
 String director;
-private Double imdbRating;
+private double imdbRating;
 String qualityOption;
 int numRatings;
 private int rate;
@@ -19,25 +19,40 @@ public Series(String director,String genre,String showName) {
     this.showName = showName;
     this.director = director;
     this.genre = genre;
-    imdbRating = 8.0;
-    seasons = 3;
-    episodes = 24; 
-    episodeOn = 0;
+    this.imdbRating = 8.0;
+    this.seasons = 3;
+    this.episodes = 24; 
+    this.episodeOn = 0;
+    this.isWatching = false;
+
 
 }
-    public void finishEpisode() {
-        episodeOn = episodeOn ++;
-         System.out.println("You are on episode " + episodeOn);
-        if (episodeOn > episodes) { 
-            episodeOn = 0;
-            seasons ++;
+    public void rateSeries(double newRating) { 
+        if (newRating >= 0 && newRating <= 10) { 
+            this.imdbRating = newRating;
+            System.out.println(showName + "rating is now updated to" + imdbRating);
+        }   else { 
+            System.out.println("This is an invalid rating! Please make your rating between 0-10" );
         }
-            
-
-    } 
-    public void isWatching() {
-        
+    }    
 
 
+    public void finishEpisode() {
+        episodeOn += 1;
+         System.out.println("You are now watching " + showName + ", episode " + episodeOn);
+
+         if (episodeOn > episodes) { 
+            episodeOn = 1;
+            seasons += 1;
+            System.out.println("Advance to season" + seasons);
+         }  else {
+            System.out.println("Episodes remaining in season: " + (episodes - episodeOn));
+         }
+        }
+    public void toggleWatchState() {
+        this.isWatching = !this.isWatching;
+    }
+    public void updateGenre(String newGenre) {
+        this.genre = newGenre;
     }
     } 
